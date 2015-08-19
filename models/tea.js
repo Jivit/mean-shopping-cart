f1b6369aec3630ee04ebda404501cbb6c5b8cb1e
Steven Abrams <steven.abrams86@gmail.com>
2015-08-19 15:09:10 -0600

    data moved into mongodb

---
+++ models/tea.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var teaSchema = new mongoose.Schema({
+  name: String,
+  ingredients: String,
+  caffeineScale: Number,
+  price: Number,
+  inStock: Boolean,
+  rating: Number,
+  imageUrl: String,
+  __v: Number,
+  categories: Array
+});
+
+var Tea = mongoose.model('Tea', teaSchema);
+
+module.exports = Tea;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/teas.json', function (req, res, next) {
+  db.Teas.find().then(function (results) {
+    res.json(results);
+  })
+})
 
 module.exports = router;
