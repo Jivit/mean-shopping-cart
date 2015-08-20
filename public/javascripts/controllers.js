2364a205ca439b86dcf4ee921cc10fcf2ce497ea
Steven Abrams <steven.abrams86@gmail.com>
2015-08-20 15:58:52 -0600

    cart crud in progress

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,6 +1,6 @@
 var cart = [];
 
-app.controller('HomeController', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+app.controller('HomeController', ['$scope', '$location', '$http', '$cookies' function ($scope, $location, $http, $cookies) {
   $http.get('/teas.json').then(function (results) {
     $scope.teas = results.data;
     $scope.categories = results.data.reduce(function (prev, curr) {
@@ -10,16 +10,30 @@ app.controller('HomeController', ['$scope', '$location', '$http', function ($sco
     })
   }, function (err) {
     console.log(err);
-  })
+  });
   $scope.cart = cart;
   $scope.goToCart = function () {
     $location.path('/cart');
   }
   $scope.addToCart = function () {
-    var item = {};
-    item = this.tea;
-    item.quantity = this.quantity ? this.quantity : 1;
-    cart.push(item);
+    if($cookies.get('cart_id')) {
+      //use existing cart if it exists
+      var userCart = $cookies.get('cart_id')
+      $http.get('/carts/'+userCart).then(function (cart) {
+        var item = {};
+        item = this.tea;
+        item.quantity = this.quantity ? this.quantity : 1;
+        cart.items.push(item);
+        return cart;
+      }).then(function (cart) {
+        $http.post('/carts/'+cart._id, {cart});
+      });
+    } else {
+      //create new cart
+      $http.post('/carts', {cart}).then(function () {
+        
+      })
+    }
   }
 }]);
 
