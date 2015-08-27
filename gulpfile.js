36d967984c55ab0e71f42f3e5f604ff0341d5c15
Steven Abrams <steven.abrams86@gmail.com>
2015-08-27 14:14:05 -0600

    working on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,16 @@ var sourcemaps = require('gulp-sourcemaps');
 var gzip = require('gulp-gzip');
 var gutil = require('gulp-util');
 var rev = require('gulp-rev');
+var minifycss = require('gulp-minify-css');
+var fingerprint = require('gulp-fingerprint');
+
 
 gulp.task('html', function () {
   return gulp.src('public/views/index.html')
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('concatScripts', function() {
+gulp.task('scripts', function() {
   return gulp.src('public/javascripts/*.js')
     .pipe(concat('all.js'))
     .pipe(uglify())
@@ -19,12 +22,28 @@ gulp.task('concatScripts', function() {
     .pipe(rev())
     .pipe(gulp.dest('dist'))
 })
-
-gulp.task('concatCss', function () {
-  return gulp.src('public/stylesheets/style.css')
+// gulp.task('js', function () {
+//   return gulp.src('js/*js')
+//     .pipe(concat('all.js'))
+//     .pipe(uglify())
+//     .pipe(rev.manifest('./'))
+//     .pipe(fingerprint())
+//     .pipe(gulp.dest('dist'))
+//     .pipe(revNapkin());
+// })
+gulp.task('css', function () {
+  return gulp.src('public/stylesheets/*.css')
+    .pipe(minifycss())
     .pipe(gzip())
     .pipe(rev())
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('default', ['html', 'concatScripts', 'concatCss'])
+gulp.task('watchout', function () {
+  gulp.watch('public/javascripts/*.js', ['scripts'])
+  gulp.watch('public/stylesheets/*.css', ['css'])
+  gulp.watch('public/views/index.html', ['html'])
+})
+
+
+gulp.task('default', ['html', 'scripts', 'css', 'watchout' ])
